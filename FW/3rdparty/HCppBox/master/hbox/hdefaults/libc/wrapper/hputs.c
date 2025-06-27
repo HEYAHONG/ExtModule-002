8fed916852d67d2333e3589c76fc104de9ece520
HEYAHONG <2229388563@qq.com>
2025-06-28 00:44:34 +0800

    更新HCppBox

--- FW/3rdparty/HCppBox/master/hbox/hdefaults/libc/wrapper/hputs.c
+++ FW/3rdparty/HCppBox/master/hbox/hdefaults/libc/wrapper/hputs.c
@@ -18,6 +18,19 @@ int hputs( const char* str)
 {
 #if defined(HPUTS)
     return HPUTS(str);
+#elif (HDEFAULTS_LIBC_OPTIMIZE_LEVEL) > 0
+    if(str!=NULL)
+    {
+        while((*str)!='\0')
+        {
+            if(hputchar(*(uint8_t *)str) < 0)
+            {
+                return -1;
+            }
+            str++;
+        }
+    }
+    return 0;
 #else
     return puts(str);
 #endif
